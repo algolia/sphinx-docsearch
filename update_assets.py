d183544ea8a9c44a5e648b277e1853b17d15e0b0
Kai Welke <kai.welke@algolia.com>
2023-07-11 11:55:57 +0200

    fix: types

--- update_assets.py
+++ update_assets.py
@@ -5,18 +5,23 @@ This extension bundles the JavaScript and CSS for DocSearch.
 This module downloads the latest version of these assets from
 the jsDelivr CDN.
 """
-import httpx
-from pathlib import Path
 import re
+from pathlib import Path
+
+import httpx
+
 
 def update_docsearch_assets() -> None:
     """Update the DocSearch assets."""
     for package in ["@docsearch/js", "@docsearch/css"]:
         _download_asset(package)
 
+
 def _download_asset(package: str) -> None:
     """Download an asset from jsDelivr."""
-    cdn_uri = f"https://cdn.jsdelivr.net/npm/{package}@latest/{_jsdelivr_file_name(package)}"
+    cdn_uri = (
+        f"https://cdn.jsdelivr.net/npm/{package}@latest/{_jsdelivr_file_name(package)}"
+    )
     response = httpx.get(cdn_uri)
     output = package.replace("@", "").replace("/", ".")
     output_path = Path("src/sphinx_docsearch/static/").resolve()
@@ -27,7 +32,9 @@ def _download_asset(package: str) -> None:
 def _jsdelivr_file_name(package: str) -> str:
     """Get the filename for an asset on jsDelivr from NPM."""
     npm_uri = f"https://registry.npmjs.org/{package}/latest"
-    return httpx.get(npm_uri).json()["jsdelivr"]
+    response = httpx.get(npm_uri)
+    return response.json()["jsdelivr"]  # type: ignore[no-any-return]
+
 
 if __name__ == "__main__":
     update_docsearch_assets()
