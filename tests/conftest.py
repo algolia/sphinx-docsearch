3c8e6d23e1443ef4880b2e25f36e0f80749552e0
Kai Welke <kai.welke@algolia.com>
2023-11-30 15:07:49 +0100

    fix: context handling

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,26 @@
 """Configuration for Pytest."""
+from typing import Any
+
 import pytest
 from _pytest.config import Config
-from sphinx.testing.path import path
+from sphinx import version_info
+
+if version_info >= (7, 2):
+    from pathlib import Path
+else:
+    from sphinx.testing.path import path
 
 pytest_plugins = "sphinx.testing.fixtures"
 collect_ignore = ["roots"]
 
 
 @pytest.fixture(scope="session")
-def rootdir() -> path:
+def rootdir() -> Any:
     """Root directory for test files."""
-    return path(__file__).parent.abspath() / "roots"
+    if version_info >= (7, 2):
+        return Path(__file__).parent.absolute() / "roots"
+    else:
+        return path(__file__).parent.abspath() / "roots"
 
 
 def pytest_configure(config: Config) -> None:
